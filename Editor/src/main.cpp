c480dbeca59ee9fc727a60c93dc0e004e2388636
SarthakKamboj <sarthakkamboj22@gmail.com>
2022-05-31 00:32:38 -0700

    misc

--- Editor/src/main.cpp
+++ Editor/src/main.cpp
@@ -143,35 +143,6 @@ int main(int argc, char* args[]) {
 	io.FontDefault = io.Fonts->AddFontFromFileTTF("assets/fonts/OpenSans-Regular.ttf", fontSize);
 
 	bool open = true;
-
-	/*
-	unsigned int fbo;
-	glGenFramebuffers(1, &fbo);
-	glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-
-	unsigned int frameBufferTex;
-	glGenTextures(1, &frameBufferTex);
-	glBindTexture(GL_TEXTURE_2D, frameBufferTex);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, frameBufferTex, 0);
-
-	unsigned int rbo;
-	glGenRenderbuffers(1, &rbo);
-	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
-
-	GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-	if (status == GL_FRAMEBUFFER_COMPLETE) {
-		std::cout << "framebuffer good to go" << std::endl;
-	}
-	else {
-		std::cout << "not good to go" << std::endl;
-	}
-	*/
-
 	FrameBuffer sceneFbo;
 
 	while (running) {
@@ -263,7 +234,6 @@ int main(int argc, char* args[]) {
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 		glViewport(0, 0, width, height);
-		// glBindFramebuffer(GL_FRAMEBUFFER, fbo);
 		sceneFbo.bind();
 		FrameBuffer::ClearBuffers();
 		glEnable(GL_DEPTH_TEST);
